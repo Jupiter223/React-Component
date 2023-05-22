7f29770d49a1bfa86e06a4d7f30589ea29050539
Jupiter <sophy8835@hotmail.com>
2023-05-22 23:28:20 +0900

    home modify

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
 
 export default function Layout() {
+
   return (
     <div>
-      LAYOUT
+     
     </div>
   )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
 
-export default function Home() {
+const Home = ({ children }) => {
+  const [isNavbarVisible, setIsNavbarVisible] = useState(true);
+
+  const handleToggleNavbar = () => {
+    setIsNavbarVisible((prevState) => !prevState);
+  };
+
   return (
     <div>
-      
+      <Navbar />
+      <div id="content" className={!isNavbarVisible ? "active" : ""}>
+        {children}
+      </div>
     </div>
-  )
-}
+  );
+};
+export default Home;
